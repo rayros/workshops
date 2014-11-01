70890f552baa1ca09033119afa059f7df9048c9d
Paweł Łaski <plaski@raffine.eu>
2014-11-01 21:54:22 +0100

    products controller: authenticate admin :create, :update, :destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :update, :destroy]
   expose(:category)
   expose(:products)
   expose(:product)
