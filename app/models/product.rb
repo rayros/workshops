188842e7be52efd9f15a962af1a747576e15097d
Paweł Łaski <plaski@raffine.eu>
2014-11-01 20:26:34 +0100

    product: val two decimal places of :price

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,6 @@ class Product < ActiveRecord::Base
   validates_presence_of :price
   validates_presence_of :description
   validates_presence_of :title
+  validates_format_of :price, :with => /\A\d+[.]\d\d\Z/, :on => :create
+
 end
