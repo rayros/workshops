e431f28dac107a3db9f864b75b531031ccda1d90
Paweł Łaski <plaski@raffine.eu>
2014-11-01 20:41:40 +0100

    product: average_rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,7 @@ class Product < ActiveRecord::Base
   validates_presence_of :title
   validates_format_of :price, :with => /\A\d+[.]?\d{1,2}\Z/, :on => :create
 
+  def average_rating
+    reviews.average(:rating)
+  end
 end
