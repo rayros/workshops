919c01ca439237881425516a1c0d16212873e743
Paweł Łaski <plaski@raffine.eu>
2014-11-01 21:58:15 +0100

    products controller: add before_action author! :edit, :update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :update, :destroy]
+  before_action :author!, only: [:edit, :update]
   expose(:category)
   expose(:products)
   expose(:product)
@@ -47,5 +48,12 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :category_id)
+  end
+
+  def author! 
+    if not self.product.user.eql? current_user
+      redirect_to category_product_url(category, product), 
+        flash: { error: 'You are not allowed to edit this product.' } and return
+    end
   end
 end
